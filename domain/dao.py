ad4b1ec25906a518f259b6518edd60136653556f
payentan <payentan@hotmail.com>
2018-05-31 09:21:25 +0800

    use lambda function

--- domain/dao.py
+++ domain/dao.py
@@ -1,4 +1,5 @@
-from persist import *
+import json
+from domain.persist import *
 
 class Node():
     def __init__(self, prev_node_list, software, algorithm, parameters, result):
@@ -7,3 +8,34 @@ class Node():
         self.algorithm = algorithm
         self.parameters =  parameters
         self.result = result
+
+class NodeSegment():
+    def __init__(self, prev_node_list, software, algorithm, parameters, result, conv_term):
+        self.prev_node_list = prev_node_list
+        self.software = software
+        self.algorithm = algorithm
+        self.parameters =  parameters
+        self.result = result
+
+        node = NodeCol()
+        node.software = software
+        node.algorithm = algorithm
+        node.parameters = json.dumps(parameters)
+        term_list = []
+
+        seqId = 0
+        for term in result:
+            seg = SegmentCol()
+            seg.seqId = seqId
+            seqId += 1
+            (seg.word, seg.nature, seg.offset) = conv_term(term)
+            seg.save()
+            term_list.append(str(seg.id))
+
+        node.result = json.dumps({'segment' : term_list })
+        node.save()
+
+        nodePrev = NodePreviousCol()
+        nodePrev.previousId = prev_node_list
+        nodePrev.nodeId = str(node.id)
+        nodePrev.save()
--- worker/segment/segmentJieba.py
+++ worker/segment/segmentJieba.py
@@ -6,30 +6,13 @@ import datetime
 import json
 
 from domain.persist import *
+from domain.dao import *
 
 def jieba_segment_persist(tid, software, algorithm, parameters, terms):
-    node = NodeCol()
-    node.software = software
-    node.algorithm = algorithm
-    node.parameters = ''
-    term_list = []
 
-    seqId = 0
-    for term in terms:
-        seg = SegmentCol()
-        seg.seqId = seqId
-        seqId = seqId + 1
-        (seg.word, seg.nature, seg.offset) = (term.word, str(term.flag), 0)
-        seg.save()
-        term_list.append(str(seg.id))
-
-    node.result = json.dumps({'segment' : term_list })
-    node.save()
-
-    nodePrev = NodePreviousCol()
-    nodePrev.previousId = tid
-    nodePrev.nodeId = str(node.id)
-    nodePrev.save()
+    NodeSegment(tid, software, algorithm, parameters, terms,
+                lambda t: (t.word, str(t.flag), 0)
+                )
 
 @pytest.fixture
 def jieba_pseg(request):
@@ -38,7 +21,12 @@ def jieba_pseg(request):
     
     for t in text:
         terms = jieba.posseg.cut(t.content)
-        def finalize():
-            jieba_segment_persist(tid, 'jieba', 'pseg', {}, terms) 
-        request.addfinalizer(finalize)
+        #def finalize():
+        #    jieba_segment_persist(tid, 'jieba', 'pseg', {}, terms) 
+        request.addfinalizer(
+            lambda :
+            NodeSegment(tid, 'jieba', 'pseg', {}, terms,
+                lambda t: (t.word, str(t.flag), 0)
+                )
+        )
         return terms
